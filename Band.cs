25ffc6629016a60c01df9340a6432998f2476170
Ross Dougherty <rossd987@gmail.com>
2014-05-19 19:22:41 -0600

    Create Repository
    Create Repository

---
+++ Band.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AmericanBlackout.Domain
+{
+    public class Band
+    {
+        public long Id { get; set; }
+        public long Name { get; set; }
+        public string Website { get; set; }
+        public string Facebook { get; set; }
+    }
+}

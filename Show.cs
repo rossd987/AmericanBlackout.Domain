f4209f644691c17a71608c4ecb13b8b6c2ded2f7
Ross Dougherty <rossd987@gmail.com>
2014-05-22 20:17:41 -0600

    add show and venue classes

---
+++ Show.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AmericanBlackout.Domain
+{
+    public class Show
+    {
+        public long Id { get; set; }
+        public DateTime Date { get; set; }
+        public long VenueId { get; set; }
+        public List<long> BandIds { get; set; }
+        public string TicketLink { get; set; }
+        public string PosterLink { get; set; }
+    }
+}

17bf5bc3b3faa76b1f05aa90464321e0d527a0a2
Ross Dougherty <rossd987@gmail.com>
2014-07-02 20:58:44 -0600

    add redis interface

---
+++ Redis/ABRedisClient.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using ServiceStack.Redis;
+using ServiceStack.Redis.Generic;
+
+
+namespace AmericanBlackout.Domain.Redis
+{
+    public class ABRedisClient : IABRedisClient
+    {
+        public IRedisClient Create()
+        {
+            return new RedisClient("nyu");
+        }
+
+        public IRedisTypedClient<T> As<T>(IRedisClient client)
+        {
+            return client.As<T>();
+        }
+    }
+}
---
+++ Redis/IABRedisClient.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using ServiceStack.Redis;
+using ServiceStack.Redis.Generic;
+
+namespace AmericanBlackout.Domain.Redis
+{
+    public interface IABRedisClient
+    {
+        IRedisClient Create();
+        IRedisTypedClient<T> As<T>(IRedisClient client);
+    }
+}
